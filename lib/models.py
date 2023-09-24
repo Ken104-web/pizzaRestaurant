daa65fc3216176f17007e9dd597bde4e47a589d5
KenCorp <kennethwarui35@gmail.com>
2023-09-25 00:36:13 +0300

    Instanciated models

--- lib/models.py
+++ lib/models.py
@@ -1,3 +1,47 @@
 from flask_sqlalchemy import SQLAlchemy
-# from sqlalchemy.orm import validates
+from sqlalchemy.orm import validates
 db = SQLAlchemy()
+
+class Restaurant(db.Model):
+    __tablename__ = 'restaurants'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    address = db.Column(db.String)
+
+    pizzas = db.relationship('Pizza', back_populates='restaurant')
+
+class Pizza(db.Model):
+    __tablename__ = 'pizzas'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    ingredients = db.Column(db.String)
+
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurants.id'))
+    restaurant = db.relationship('Restaurant', backref='pizza')
+
+class RestaurantPizza(db.Model):
+    __tablename__ = 'restaurant_pizzas'
+    id = db.Column(db.Integer, primary_key=True)
+    price = db.Column(db.Numeric(precision=3, asdecimal=True, decimal_return_scale=None))
+
+    pizza_id = db.Column(db.Integer, db.ForeignKey('pizzas.id'))
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurants.id'))
+
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    @validates('price')
+    def validate_restaurantPizza_price(self, key, value):
+        if 1 <= value <= 30:
+            return value
+        else:
+            raise ValueError('Price is not within the required range')
+
+    @validates('name')
+    def validate_pizza_name(self, key, value):
+        if len(value) >= 50:
+            raise ValueError('Exceeded charr limit for pizza name')
+        return value
